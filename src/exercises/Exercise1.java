f3d6a683071a4021297705ee35860e84605e961c
Pedro Lopez <plopez@bmj.com>
2018-03-28 16:15:13 +0100

    Initial commit

---
+++ src/exercises/Exercise1.java
@@ -0,0 +1,26 @@
+package exercises;
+
+import java.util.*;
+
+public class Exercise1
+{
+    static int getSockPairs(int n, int[] ar)
+    {
+        // Complete this function
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] c = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            c[i] = in.nextInt();
+        }
+        int result = getSockPairs(n, c);
+        System.out.println(result);
+        in.close();
+    }
+}
